f02e36754175ee14af95130d9339e74a906d544e
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-08-16 12:09:03 -0500

    Update tests for the operate file

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -1,19 +1,19 @@
 import operate from './operate';
 
 describe('Do operations +, -, *, ÷, and %', () => {
-  test('sum 90 + 100 to equal 190', () => {
+  test('sum 90 + 100 should be equal 190', () => {
     expect(operate(90, 100, '+')).toBe('190');
   });
-  test('substract 9 - 8 to equal 1', () => {
+  test('substract 9 - 8 should be equal to 1', () => {
     expect(operate(9, 8, '-')).toBe('1');
   });
-  test('multiply 8 * 9 to equal 72', () => {
+  test('multiply 8 * 9 should be equal to 72', () => {
     expect(operate(8, 9, 'x')).toBe('72');
   });
-  test('divide 30 ÷ 10 to equal 3', () => {
+  test('divide 30 ÷ 10 should be equal to 3', () => {
     expect(operate(3, 1, '÷')).toBe('3');
   });
-  test('module 10 % 4 to equal 2', () => {
+  test('module 10 % 4 should be equal to 2', () => {
     expect(operate(10, 4, '%')).toBe('2');
   });
 });
