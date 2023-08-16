2db6ec0784b106ea438536a3753a60798f89e662
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-08-15 20:45:19 -0500

    Add routing to navigate between the 3 pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import { Route, Routes, NavLink } from 'react-router-dom';
 import './App.css';
-import Calculator from './components/Calculator';
-import Quote from './components/Quote';
+import CalculatorItem from './components/CalculatorItem';
+import QuotesItem from './components/QuotesItem';
+import Home from './components/Home';
+
+const links = [
+  { path: '/', text: 'Home' },
+  { path: 'calculator', text: 'Calculator' },
+  { path: 'quotes', text: 'Quotes' },
+];
+
+const titleStyles = { color: '#000', fontSize: '35px' };
 
 const App = () => (
   <div className="main-container">
-    <Calculator />
-    <Quote />
+    <nav>
+      <h1 style={titleStyles}>Math Magician</h1>
+      <ul>
+        {links.map((link) => (
+          <li key={link.text}>
+            <NavLink to={link.path}>{link.text}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+    <section>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<CalculatorItem />} />
+        <Route path="/quotes" element={<QuotesItem />} />
+      </Routes>
+    </section>
   </div>
 );
 
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+const Home = () => (
+  <div className="main-home">
+    <p>This calculator app provides simple mathematical functions in a beautifully designed app.</p>
+    <p>
+      <span>• Perform basic calculations such as addition, subtraction,</span>
+      <span> multiplication, division and porcentage.</span>
+    </p>
+    <p>
+      <span>• Find a wide range of inspirational succes quotes if you</span>
+      <span> need an extra push to get stuff done.</span>
+    </p>
+  </div>
+);
+
+export default Home;
