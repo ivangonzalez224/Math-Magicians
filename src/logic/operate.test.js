c8d1c8c7bac9772293861cc47a759c38b2837a7a
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-08-17 07:45:06 -0500

    Test scenarios when dividing by o and for unknown operation

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -16,4 +16,15 @@ describe('Do operations +, -, x, ÷, and %', () => {
   test('module 10 % 4 should be equal to 2', () => {
     expect(operate(10, 4, '%')).toBe('2');
   });
+  test('the return should be "Can\'t divide by 0."  for the division of 20 by 0', () => {
+    expect(operate('20', '0', '÷')).toEqual('Can\'t divide by 0.');
+   });
+   
+   test('the return should be "Can\'t find modulo as can\'t divide by 0." for the modulo of 5 and 0', () => {
+      expect(operate('5', '0', '%')).toEqual('Can\'t find modulo as can\'t divide by 0.');
+   });
+   
+   test('the output should throw error for unknown operation', () => {
+    expect(() => operate('15', '3', '@')).toThrow('Unknown operation \'@\'');
+   });
 });
